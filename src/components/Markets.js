0be8230242aa23c8636c4489f6d3084f3a33ed64
Gopal Karan <gopalkaran@krishworks.com>
2022-02-13 18:10:45 +0530

    removed extra lines

--- src/components/Markets.js
+++ src/components/Markets.js
@@ -1,42 +1,50 @@
-import React,{useState, useEffect} from 'react'
-import axios from 'axios';
-import Table from './Table';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Table from "./Table";
+import Pagination from "./Pagination";
 
 const Markets = () => {
-    const [markets, setMarkets] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage, setPostsPerPage] = useState(20)
-    useEffect(() => {
-        fetchItems();
-      }, []);
-    
-      const fetchItems = async () => {
-        setLoading(true);
-        axios
-          .get("https://api.coincap.io/v2/markets")
-          .then(function (response) {
-            console.log(response.data.data);
-            setMarkets(response.data.data)
-            setLoading(false);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      };
+  const [markets, setMarkets] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(20);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
+  const fetchItems = async () => {
+    setLoading(true);
+    axios
+      .get("https://api.coincap.io/v2/markets")
+      .then(function (response) {
+        console.log(response.data.data);
+        setMarkets(response.data.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-            // Get current posts
-            const indexOfLastPost = currentPage * postsPerPage;
-            const indexOfFirstPost = indexOfLastPost - postsPerPage;
-            const currentMarkets = markets.slice(indexOfFirstPost, indexOfLastPost)
-    
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentMarkets = markets.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change Page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
     <div>
-        <input type='search'  />
-        <Table markets={currentMarkets} loading={loading} />
+      <input type="search" />
+      <Table markets={currentMarkets} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={markets.length}
+        paginate={paginate}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Markets
+export default Markets;
--- src/components/Prices.js
+++ src/components/Prices.js
@@ -1,46 +1,50 @@
-import React, {useEffect, useState} from 'react'
-import axios from 'axios';
-import PriceTable from './PriceTable';
-import Pagination from './Pagination';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import PriceTable from "./PriceTable";
+import Pagination from "./Pagination";
 
 const Prices = () => {
-    const [prices, setPrices] = useState([])
-    const [loading, setLoading] = useState(false)
-    const [currentPage, setCurrentPage] = useState(1)
-    const [postsPerPage, setPostsPerPage] = useState(20)
-    useEffect(() => {
-        fetchItems();
-      }, []);
-    
-      const fetchItems = async () => {
-        setLoading(true);
-        axios
-          .get("https://api.coincap.io/v2/rates")
-          .then(function (response) {
-            console.log(response.data.data);
-            setPrices(response.data.data)
-            setLoading(false);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-      };
+  const [prices, setPrices] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(20);
+  useEffect(() => {
+    fetchItems();
+  }, []);
 
-      // Get current posts
-      const indexOfLastPost = currentPage * postsPerPage;
-      const indexOfFirstPost = indexOfLastPost - postsPerPage;
-      const currentPrices = prices.slice(indexOfFirstPost, indexOfLastPost)
+  const fetchItems = async () => {
+    setLoading(true);
+    axios
+      .get("https://api.coincap.io/v2/rates")
+      .then(function (response) {
+        console.log(response.data.data);
+        setPrices(response.data.data);
+        setLoading(false);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
 
-      // Change Page
-      const paginate = (pageNumber) => setCurrentPage(pageNumber);
-    
+  // Get current posts
+  const indexOfLastPost = currentPage * postsPerPage;
+  const indexOfFirstPost = indexOfLastPost - postsPerPage;
+  const currentPrices = prices.slice(indexOfFirstPost, indexOfLastPost);
+
+  // Change Page
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
+
   return (
-    <div style={{display: 'grid'}}>
-        <input type='search' />
-        <PriceTable prices={currentPrices} loading={loading} />
-        <Pagination postsPerPage={postsPerPage} totalPosts={prices.length} paginate={paginate} />
+    <div style={{ display: "grid" }}>
+      <input type="search" />
+      <PriceTable prices={currentPrices} loading={loading} />
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={prices.length}
+        paginate={paginate}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Prices
+export default Prices;
